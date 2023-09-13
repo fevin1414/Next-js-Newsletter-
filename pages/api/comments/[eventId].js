3c956b5def4dab5ab7c58b335fe1151cdf756e32
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 09:46:04 -0500

    added custom error message

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,47 +1,55 @@
-import {MongoClient} from 'mongodb';
-export async function handler(req,res){
+import { MongoClient } from 'mongodb';
 
-  const eventId=req.query.eventId;
-const client=await MongoClient.connect( 'mongodb+srv://fevin:Fevin4321@cluster0.vm8ghse.mongodb.net/events?retryWrites=true&w=majority');
-  if (req.method === 'POST') {
-    const { email, name, text } = req.body;
+export async function handler(req, res) {
+  const eventId = req.query.eventId;
+  let client;
 
-    if (
-      !email.includes('@') ||
-      !name ||
-      name.trim() === '' ||
-      !text ||
-      text.trim() === ''
-    ) {
-      res.status(422).json({ message: 'Invalid input.' });
-      return;}
-      console.log(email,name,text);
-      const newComment={
+  try {
+    client = await MongoClient.connect(
+      'mongodb+srv://fevin:Fevin4321@cluster0.vm8ghse.mongodb.net/events?retryWrites=true&w=majority'
+    );
 
+    if (req.method === 'POST') {
+      const { email, name, text } = req.body;
+
+      if (
+        !email.includes('@') ||
+        !name ||
+        name.trim() === '' ||
+        !text ||
+        text.trim() === ''
+      ) {
+        res.status(422).json({ message: 'Invalid input.' });
+        return;
+      }
+
+      const newComment = {
         email,
         name,
         text,
-        eventId
-      }
-      const db=client.db();
-   const result=await db.collection('comments').insertOne(newComment);
+        eventId,
+      };
 
+      const db = client.db();
+      const result = await db.collection('comments').insertOne(newComment);
+      newComment.id = result.insertedId;
 
-      console.log(newComment);
-      newComment.id=result.insertedId;
-
-      res.status(201).json({message:'added Comment.'});
+      res.status(201).json({ message: 'Added Comment.', comment: newComment });
     }
 
- if(req.method === 'GET'){
-
- const db=client.db();
-
-const documents=await db.collection('comments').find().sort({_id:-1}).toArray();
-res.status(201).json({comments:documents});
-
- }
- client.close();
+    if (req.method === 'GET') {
+      const db = client.db();
+      const documents = await db.collection('comments').find().sort({ _id: -1 }).toArray();
+      res.status(200).json({ comments: documents });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'An error occurred while processing your request' });
+  } finally {
+    if (client) {
+      client.close();
+    }
+  }
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,20 +1,29 @@
-import {MongoClient} from 'mongodb'
-async  function handler(req,res){
-  if(req.method === 'POST'){
-   const userEmail= req.body.email;
+import { MongoClient } from 'mongodb';
 
-   if(!userEmail || ! userEmail.includes('@')){
-    res.status(422).json({message:'Invalid email address'})
-    return;
-   }
-   const client = await MongoClient.connect(
-    'mongodb+srv://fevin:Fevin4321@cluster0.vm8ghse.mongodb.net/newsletter?retryWrites=true&w=majority'
-  );
- const db=  client.db();
-   await db.collection('newsletter').insertOne({email:userEmail});
-   client.close();
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
 
-   res.status(201).json({message: 'Signed Up'})
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address' });
+      return;
+    }
+
+    try {
+      const client = await MongoClient.connect(
+        'mongodb+srv://fevin:Fevin4321@cluster0.vm8ghse.mongodb.net/test?retryWrites=true&w=majority'
+      );
+      const db = client.db();
+      await db.collection('newsletter').insertOne({ email: userEmail });
+      client.close();
+
+      res.status(201).json({ message: 'Signed Up' });
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Connection error' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
 }
 
